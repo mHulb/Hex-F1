62ba05086906aa91e789060e10b9193ea5b79804
Jan <jan.niklas.bluhm@gmail.com>
2016-07-12 16:37:06 +0200

    formatierungen und testdatei verbessert

--- pp2/Hex_KI.py
+++ pp2/Hex_KI.py
@@ -12,7 +12,8 @@ class HexKI:
         """
         self.m = m  # number of rows
         self.n = n  # number of columns
-        self.first_move = 1
+        self.move_number = 1
+        self.moves = None
         self.nodes = [[AINode(i, j) for j in range(m)] for i in range(n)]
         # boundary nodes have no indices, but a predefined colour (1 or 2)
         self.boundaries = {1: [AINode(None, None, 1), AINode(None, None, 1)],
@@ -28,13 +29,13 @@ class HexKI:
         self.opponent_colour = None
 
     def __make_edges(self):
-        edges = [] # logisch gesehen waere ein set wohl sinnvoller
+        edges = []
         for row in self.nodes:
             for node in row:
                 for neighbour in node.neighbours:
                     # resistance default value is 2 for both sides
                     new_edge = Edge(node, neighbour)
-                    if not new_edge in edges:
+                    if new_edge not in edges:
                         edges.append(new_edge)
 
                         # add edge to both nodes
@@ -44,7 +45,6 @@ class HexKI:
         for edge in edges:
             edge.update_resistances()
         return edges
-
 
     def __initialize_nodes(self, m, n):
         # initializing the basic board nodes
@@ -66,10 +66,10 @@ class HexKI:
         # initializing the boundary nodes
         upper_bound = self.boundaries[1][0]
         lower_bound = self.boundaries[1][1]
-        for node in self.nodes[0]: # upper side
+        for node in self.nodes[0]:      # upper side
             node.neighbours.append(upper_bound)
             upper_bound.neighbours.append(node)
-        for node in self.nodes[n - 1]: # lower side
+        for node in self.nodes[n - 1]:  # lower side
             node.neighbours.append(lower_bound)
             lower_bound.neighbours.append(node)
 
@@ -98,59 +98,63 @@ class HexKI:
         mo = {}
         nodes = self.nodes
 
-
         # Nachfolgende ist zum Speichern der besten moves gedacht
         # Wenn man mit besseren moves anfängt, spart man sich angeblich zeit
 
         # Beim ersten Zug werden nur das mitlere quadrat durchsucht
         # muss noch angepasst werden mit swap später
-        if self.first_move == 1:
+        if self.move_number == 1:
             self.moves = {}
             # noch in list comprehension
             for i in range(1, self.n - 1):
-                for j in range(1,self.m-1):
+                for j in range(1, self.m - 1):
                     (self.moves.setdefault(1, [])).append((i, j))
 
-            self.first_move += 1
+            self.move_number += 1
 
         # Beim zweiten move werden alle fehlenden moves hinzugefuegt
-        elif self.first_move == 2:
+        elif self.move_number == 2:
             for i in range(self.n):
                 for j in range(self.m):
-                    if i not in range(1, self.n-1) or not j in range(1,self.m-1):
+                    if i not in range(1, self.n - 1) or j not in range(1, self.m - 1):
                         (self.moves.setdefault(1, [])).append((i, j))
-            self.first_move += 1
+            self.move_number += 1
 
-        # Sortiere Moves nach a wert, so das er mit dem kleinsten a
-        # beginnt(kleines a -> guter move)
+        # Sortiere Moves nach a wert, so dass er mit dem kleinsten a
+        # beginnt (kleines a -> guter move)
         for val in sorted(self.moves):
             if val == 0 and len(self.moves[val]) > 1:
                 moves = [self.moves[val]]
+                val = 0  # as integer
             else:
                 moves = self.moves
 
-            for move in moves[val]:
-                if nodes[move[0]][move[1]].colour == 0:
-                    # Zum probieren des jeweiligen moves muss die Farbe geändert werden
+            for i, j in moves[val]:
+                if nodes[i][j].colour == 0:
+                    # Zum probieren des jeweiligen moves muss die Farbe
+                    # geändert werden.
                     # Daher tmporere nodes
-                    nodes[move[0]][move[1]].change_colour(self.player_colour)
+                    nodes[i][j].change_colour(self.player_colour)
                     # theoretisch muesste hier min_value aufgerufen werden
                     a = self.min_value(nodes, float("inf"), -float("inf"), 2)
-                    # wieder zurueck setzten, damit es beim naechsten move nicht
-                    # stoert
-                    nodes[move[0]][move[1]].change_colour(0)
-                    nodes[move[0]][move[1]].pot = 1
+                    # wieder zurueck setzten, damit es beim naechsten move
+                    # nicht stoert
+                    nodes[i][j].change_colour(0)
+
+                    # ?Frage? Warum wird potential auf 1 gesetzt?
+                    nodes[i][j].pot = 1
 
                     # Moves für die naechste Runde abspeichern
-                    (mo.setdefault(a, [])).append((move[0], move[1]))
+                    (mo.setdefault(a, [])).append((i, j))
 
-                    show_board[move[0]][move[1]] = round(a,3)
+                    show_board[i][j] = round(a, 3)
                     if a < mini:
                         mini = a
-                        self.best_move = move
+                        self.best_move = (i, j)
         self.moves = mo
         # Ausgabe der a Werte in Matrixform
-        print(' \n'.join('       '.join(str(a) for a in row) for row in show_board))
+        print(' \n'.join(
+            '       '.join(str(a) for a in row) for row in show_board))
 
         return True
 
@@ -160,7 +164,7 @@ class HexKI:
         """
         if not nodes:
             nodes = self.nodes
-        #if not edges:
+        # if not edges:
         #    edges = self.edges
         start_node = self.boundaries[self.player_colour][0]
         end_node = self.boundaries[self.player_colour][1]
@@ -172,28 +176,30 @@ class HexKI:
         end_node = self.boundaries[self.opponent_colour][1]
         board_eval_2 = Dijkstra(nodes, start_node, end_node)
         value_2 = board_eval_2.value
+        """
         if value_1 == 0:
-            return 0
-        elif value_2 == 0:
+            return 0 """
+
+        # edge case division by zero
+        if value_2 == 0:
             return float("inf")
-        return value_1 / value_2
+        else:
+            return value_1 / value_2
 
     def nextMove(self):
         """
         """
-        self.nodes[self.best_move[0]][self.best_move[1]].change_colour(self.player_colour)
-
-        for row in self.nodes:
-            print(' '.join(str(node) for node in row)),
-
+        best_i, best_j = self.best_move
+        self.nodes[best_i][best_j].change_colour(
+            self.player_colour)
 
         return self.best_move
 
     def receiveMove(self, move):
         """
         """
-        self.nodes[move[0]][move[1]].change_colour(self.opponent_colour)
-
+        i, j = move
+        self.nodes[i][j].change_colour(self.opponent_colour)
 
     def readBoard(self, board, current=True):
         """
@@ -204,8 +210,8 @@ class HexKI:
 
     def random_move(self):
         while True:
-            i = random.randint(0, self.m-1)
-            j = random.randint(0, self.n-1)
+            i = random.randint(0, self.m - 1)
+            j = random.randint(0, self.n - 1)
 
             if self.nodes[i][j].colour == 0:
                 return (i, j)
@@ -214,42 +220,46 @@ class HexKI:
 
         if (depth == 0):
             return self.evaluate(nodes)
-        #moves = [(i, j) for i in range(self.n) for j in range(self.m)]
-        #for move in moves:
+        # moves = [(i, j) for i in range(self.n) for j in range(self.m)]
+        # for move in moves:
         for val in sorted(self.moves):
-            for move in self.moves[val]:
-                if nodes[move[0]][move[1]].colour == 0:
-                    nodes[move[0]][move[1]].change_colour(self.player_colour)
-
+            for i, j in self.moves[val]:
+                if nodes[i][j].colour == 0:
+                    nodes[i][j].change_colour(self.player_colour)
 
                     a = min(a, self.min_value(nodes, a, b, depth - 1))
 
-                    nodes[move[0]][move[1]].change_colour(0)
-                    nodes[move[0]][move[1]].pot = 1
-
+                    nodes[i][j].change_colour(0)
+                    nodes[i][j].pot = 1
 
         # this ia a cutoff point
         if a <= b:
-             return a
+            return a
         return a
 
     def min_value(self, nodes, a, b, depth):
         if (depth == 0):
             return self.evaluate(nodes)
-        #moves = [(i, j) for i in range(self.n) for j in range(self.m)]
-        #for move in moves:
+        # moves = [(i, j) for i in range(self.n) for j in range(self.m)]
+        # for move in moves:
         for val in sorted(self.moves):
-            for move in self.moves[val]:
-                if nodes[move[0]][move[1]].colour == 0:
-                    nodes[move[0]][move[1]].change_colour(self.opponent_colour)
+            for i, j in self.moves[val]:
+                if nodes[i][j].colour == 0:
+                    nodes[i][j].change_colour(self.opponent_colour)
 
                     b = max(b, self.max_value(nodes, a, b, depth - 1))
-                    nodes[move[0]][move[1]].change_colour(0)
-                    nodes[move[0]][move[1]].pot = 1
-
+                    nodes[i][j].change_colour(0)
+                    nodes[i][j].pot = 1
 
         # this is a cutoff point
         if b >= a:
             return b
         return b
 
+    # String representation to test logic without GUI dependency
+    def __str__(self):
+        colours = [[node.string_rep() for node in row] for row in self.nodes]
+        output = ""
+        for i, row in enumerate(colours):
+            output += i * "   " + "   ".join(row) + "\n"
+        return output
--- pp2/temp_tester.py
+++ pp2/temp_tester.py
@@ -1,66 +1,30 @@
 from Hex_KI import HexKI
+from Hex import HexBoard
 import time
 
+m, n = 5, 5
+testKI = HexKI(m, n)
+board = HexBoard(m, n)
+player_colour = 1
+opponent_colour = 2
+testKI.player_colour = player_colour
+testKI.opponent_colour = opponent_colour
 
-
-testKI = HexKI(4, 4)
-testKI.player_colour = 1
-testKI.opponent_colour = 2
-
-
-
-# testKI.best_move = (0,0)
-# testKI.nextMove()
-# testKI.best_move = (1,0)
-# testKI.nextMove()
-#
-# testKI.calculateMove()
-
-
-# testKI.best_move = (0,2)
-# testKI.nextMove()
-# testKI.calculateMove()
-#
-# testKI.best_move = (0,3)
-# testKI.nextMove()
-for i in range(10):
-    testKI.receiveMove(testKI.random_move())
+while not board.finished():
+    random_move = testKI.random_move()
+    print("Random move {}".format(random_move))
+    testKI.receiveMove(random_move)
+    board.receiveMove(random_move, opponent_colour)
+    # print(testKI)
+    print(board)
     t0 = time.clock()
     testKI.calculateMove()
-    print(time.clock() - t0)
-    testKI.nextMove()
+    calculated_move = testKI.nextMove()
+    print("Calculation for move {1} took {0} seconds.".format(
+        time.clock() - t0, calculated_move))
+    board.receiveMove(calculated_move, player_colour)
+    # print(testKI)
+    print(board)
 
-
-
-
-# for edge in testKI.edges:
-#     print(edge)
-# testKI.player_colour = 1
-# print(testKI.evaluate())
-
-print(testKI.evaluate())
-
-
-testKI.opponent_colour = 2
-move = (1, 1)
-print("Make move {}".format(move))
-testKI.receiveMove((move))
-
-testKI.calculateMove()
-testKI.nextMove()
-
-print(testKI.evaluate())
-
-testKI.opponent_colour = 1
-move = (1, 2)
-print("Make move {}".format(move))
-testKI.receiveMove((move))
-
-testKI.calculateMove()
-testKI.nextMove()
-
-testKI.player_colour = 1
-testKI.opponent_colour = 2
-
-print(testKI.evaluate())
+print("Player {} won the game!".format(board.winner()))
 
